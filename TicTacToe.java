2bcd566d580cbac2351b8b47551c8bc69aa44eca
Max N <71468091+minetter@users.noreply.github.com>
2024-04-25 13:10:37 -0700

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,9 +4,14 @@
     The TicTacToe class should have a 2D array as an instance variable and a constructor that initializes the 2D array with the "-" value.
 
     Add a getter method that returns the private 2D instance variable.*/
-public class TicTacToe
-{
-    String[][] boardList = {{"-", "-", "-"},{"-", "-", "-"},{"-", "-", "-"}};
+public class TicTacToe {
+private String[][] board;
 
-
+public TicTacToe() {
+    board = new String[3][3];
+    for (int row = 0; row < 3; row++) {
+        for (int play = 0; play < 3; play++) {
+            board[row][play] = "-";
+        }
+    }
 }
